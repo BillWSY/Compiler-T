6eba8a164fc1bbd487cf18992b8abd022d0daa56
YanBo Bai <baiyb92@163.com>
2013-06-11 23:35:40 +0800

    new edition

--- ParseTree.cpp
+++ ParseTree.cpp
@@ -6,12 +6,12 @@
 #include <sstream>
 
 using namespace std;
- 
+
 class BasicNode {
 public:
-   int kind;      //种类信息~~ 
-   int total_node;  //当前结点总子树大小 
-   string data_for_print; //用来输出的信息 
+   int kind;      //种类信息~~
+   int total_node;  //当前结点总子树大小
+   string data_for_print; //用来输出的信息
 };
 
 /*--------------------exp---------------------------------*/
@@ -21,7 +21,7 @@ public:
        string name;
        NId(string _name) {
                       name = _name;
-                      kind = 1;  
+                      kind = 1;
                       data_for_print = "Identifier " + name;
        }
 };
@@ -31,11 +31,11 @@ class Expression1: public BasicNode{
 public:
        BasicNode *lvalue;
        Expression1(BasicNode *_lvalue) {
-                lvalue = _Lvalue;
+                lvalue = _lvalue;
                 kind = 2;
-       }        
-           
-}
+       }
+
+};
 
 /*TOK_INTEGER 3*/
 class Expression2: public BasicNode {
@@ -44,8 +44,8 @@ public:
        Expression2(int _value){
                     value = _value;
                     kind = 3;
-                    data_for_print = "Integer " + value.str();
-       }            
+                    data_for_print = "Integer ";
+       }
 };
 
 /*TOK_Nil 4*/
@@ -54,7 +54,7 @@ public:
        Expression3() {
                     kind = 4;
                     data_for_print = "Nil";
-       }            
+       }
 };
 
 /*TOK_STRING 5*/
@@ -64,9 +64,9 @@ public:
        Expression4(string _value) {
                       value = _value;
                       kind = 5;
-                      data_for_print = "String " + value;
+                      data_for_print = "String ";
        }
-};  
+};
 
 /*TOK_STRING 6 id(<ArgList>)*/
 class Expression5: public BasicNode {
@@ -77,23 +77,23 @@ public:
                       arglist = _arglist;
                       kind = 6;
        }
-};  
+};
 
-/*TOK_BinOp  7 把几种都合起来了*/ 
+/*TOK_BinOp  7 把几种都合起来了*/
 class Expression6: public BasicNode {
 public:
        string op;
        BasicNode *exp1,*exp2;
        Expression6(string _op,BasicNode *_exp1,BasicNode *_exp2) {
                       op = _op;
-                      exp1 = _exp1; 
+                      exp1 = _exp1;
                       exp2 = _exp2;
-                      kind = 7; 
+                      kind = 7;
                       data_for_print = "BinOp ";
        }
 };
 
-/*TOK_UnOp  8*/ 
+/*TOK_UnOp  8*/
 class Expression7: public BasicNode {
 public:
        string op;
@@ -107,7 +107,7 @@ public:
 };
 
 
-/* 9 id{FieldExpList} */ 
+/* 9 id{FieldExpList} */
 class Expression8: public BasicNode {
 public:
        BasicNode *id, *fieldexplist;
@@ -119,7 +119,7 @@ public:
        }
 };
 
-/* 10 (ExpList) */ 
+/* 10 (ExpList) */
 class Expression9: public BasicNode {
 public:
        BasicNode *explist;
@@ -130,11 +130,11 @@ public:
        }
 };
 
-/* 11  LValue := <Exp>*/ 
+/* 11  LValue := <Exp>*/
 class Expression10: public BasicNode {
 public:
        BasicNode *lvalue,*exp;
-       Expression10(BasicNode *_lvalue,*exp) {
+       Expression10(BasicNode *_lvalue,BasicNode *_exp) {
                       exp = _exp;
                       lvalue = _lvalue;
                       kind = 11;
@@ -142,11 +142,11 @@ public:
        }
 };
 
-/* 12  if <Exp> then <Exp>*/ 
+/* 12  if <Exp> then <Exp>*/
 class Expression11: public BasicNode {
 public:
        BasicNode *exp1,*exp2;
-       Expression11(BasicNode *_exp1,*_exp2) {
+       Expression11(BasicNode *_exp1,BasicNode *_exp2) {
                       exp1 = _exp1;
                       exp2 = _exp2;
                       kind = 12;
@@ -154,11 +154,11 @@ public:
        }
 };
 
-/* 13  if <Exp> then <Exp> else <Exp>*/ 
+/* 13  if <Exp> then <Exp> else <Exp>*/
 class Expression12: public BasicNode {
 public:
        BasicNode *exp1,*exp2,*exp3;
-       Expression12(BasicNode *_exp1,*_exp2,*_exp3) {
+       Expression12(BasicNode *_exp1,BasicNode *_exp2,BasicNode *_exp3) {
                       exp1 = _exp1;
                       exp2 = _exp2;
                       exp3 = _exp3;
@@ -167,11 +167,11 @@ public:
        }
 };
 
-/* 14   while <Exp> do <Exp>*/ 
+/* 14   while <Exp> do <Exp>*/
 class Expression13: public BasicNode {
 public:
        BasicNode *exp1,*exp2;
-       Expression13(BasicNode *_exp1,*_exp2) {
+       Expression13(BasicNode *_exp1,BasicNode *_exp2) {
                       exp1 = _exp1;
                       exp2 = _exp2;
                       kind = 14;
@@ -179,11 +179,11 @@ public:
        }
 };
 
-/* 15   for id := <Exp> to <Exp> do <Exp>*/ 
+/* 15   for id := <Exp> to <Exp> do <Exp>*/
 class Expression14: public BasicNode {
 public:
        BasicNode *id,*exp1,*exp2;
-       Expression14(BasicNode *_id,*_exp1,*_exp2) {
+       Expression14(BasicNode *_id,BasicNode *_exp1,BasicNode *_exp2) {
                       id = _id;
                       exp1 = _exp1;
                       exp2 = _exp2;
@@ -198,7 +198,7 @@ public:
        Expression15() {
                     kind = 16;
                     data_for_print = "Break";
-       }            
+       }
 };
 
 
@@ -211,26 +211,88 @@ public:
                     explist = _explist;
                     kind = 17;
                     data_for_print = "let Statement";
-       }            
+       }
 };
 
 /* 18  id[Exp] of Exp  这条好像有点不一致，我看到的原版是这个..*/
 class Expression17: public BasicNode {
 public:
-       BasicNode *id,*exp1,*;
+       BasicNode *id,*exp1,*exp2;
        Expression17(BasicNode*_id,BasicNode*_exp1,BasicNode*_exp2) {
                     id = _id;
                     exp1 = _exp1;
                     exp2 = _exp2;
                     kind = 18;
                     data_for_print = "Of Statement";
-       }            
+       }
 };
 /*--------------------end of exp---------------------------------*/
- 
+
 /* 19  None */
-class None:public BasicNode {
-      None() {kind = 19;  data_for_print = "NONE" }
+class None: public BasicNode {
+      None() {kind = 19;  data_for_print = "NONE"; }
+};
+
+/*20  <DecList>      ->        <Dec> <Declist>*/
+class DecList1:public BasicNode {
+    BasicNode* dec, *decList;
+    DecList1(BasicNode *_dec, BasicNode *_declist) {
+        dec = _dec;
+        decList = _declist;
+        kind = 20;
+        data_for_print = "DecList"
+    }
+};
+
+/*21 DecList ->  None*/
+class DecList2:public BasicNode {
+    BasicNode* none;
+    DecList2(BasicNode *_none)
+        node = _none;
+        kind = 21;
+        data_for_print = "DecList"
+    }
+};
+
+/*22 Dec ->  TyDec*/
+class DecNode1:public BasicNode {
+    BasicNode* tydec;
+    DecNode1(BasicNode *_tydec)
+        tydec = _tydec;
+        kind = 22;
+        data_for_print = "Dec(TyDec)"
+    }
+};
+
+/*23 Dec ->  VarDec*/
+class DecNode2:public BasicNode {
+    BasicNode* vardec;
+    DecNode2(BasicNode *_vardec)
+        vardec = _vardec;
+        kind = 23;
+        data_for_print = "Dec(VarDec)"
+    }
+};
+
+/*24 Dec ->  FunDec*/
+class DecNode2:public BasicNode {
+    BasicNode* fundec;
+    DecNode2(BasicNode *_fundec)
+        fundec = _fundec;
+        kind = 24;
+        data_for_print = "Dec(FunDec)"
+    }
+};
+
+
+int main() {
+    Expression4* temp = new Expression4("ok~~~~");
+    cout << temp->value;
+    BasicNode * aa = temp;
+    Expression4* now = (Expression4*)(aa);
+    printf("%s",now->value.c_str());
+    int x;
+    while (true) cin>>x;
 }
 
 
